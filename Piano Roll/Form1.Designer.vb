e1ed657c310c1bde29bb7f559bde4aef5ddf6e9a
EarthenSky <127-gstang@sd43.bc.ca>
2017-03-10 11:55:12 -0800

    more stuff
    yup

--- Piano Roll/Form1.Designer.vb
+++ Piano Roll/Form1.Designer.vb
@@ -22,45 +22,87 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.pnlGrid = New System.Windows.Forms.Panel()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.pnlGrid.SuspendLayout()
+        Me.btnLeft = New System.Windows.Forms.Button()
+        Me.btnRight = New System.Windows.Forms.Button()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.tbxDebugLog = New System.Windows.Forms.TextBox()
+        Me.pbxGrid = New System.Windows.Forms.PictureBox()
+        CType(Me.pbxGrid, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'pnlGrid
+        'btnLeft
         '
-        Me.pnlGrid.BackColor = System.Drawing.SystemColors.ControlDark
-        Me.pnlGrid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.pnlGrid.Controls.Add(Me.Button1)
-        Me.pnlGrid.Location = New System.Drawing.Point(12, 11)
-        Me.pnlGrid.Name = "pnlGrid"
-        Me.pnlGrid.Size = New System.Drawing.Size(1170, 553)
-        Me.pnlGrid.TabIndex = 0
+        Me.btnLeft.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLeft.Location = New System.Drawing.Point(12, 497)
+        Me.btnLeft.Name = "btnLeft"
+        Me.btnLeft.Size = New System.Drawing.Size(36, 32)
+        Me.btnLeft.TabIndex = 1
+        Me.btnLeft.Text = "<"
+        Me.btnLeft.UseVisualStyleBackColor = True
         '
-        'Button1
+        'btnRight
         '
-        Me.Button1.Location = New System.Drawing.Point(426, 241)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnRight.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRight.Location = New System.Drawing.Point(54, 497)
+        Me.btnRight.Name = "btnRight"
+        Me.btnRight.Size = New System.Drawing.Size(36, 32)
+        Me.btnRight.TabIndex = 2
+        Me.btnRight.Text = ">"
+        Me.btnRight.UseVisualStyleBackColor = True
+        '
+        'btnPlay
+        '
+        Me.btnPlay.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlay.Location = New System.Drawing.Point(96, 497)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(106, 32)
+        Me.btnPlay.TabIndex = 3
+        Me.btnPlay.Text = "Play"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'tbxDebugLog
+        '
+        Me.tbxDebugLog.Location = New System.Drawing.Point(208, 497)
+        Me.tbxDebugLog.Multiline = True
+        Me.tbxDebugLog.Name = "tbxDebugLog"
+        Me.tbxDebugLog.Size = New System.Drawing.Size(764, 32)
+        Me.tbxDebugLog.TabIndex = 4
+        Me.tbxDebugLog.Text = "5 octaves, B7 - B2"
+        '
+        'pbxGrid
+        '
+        Me.pbxGrid.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.pbxGrid.Location = New System.Drawing.Point(12, 12)
+        Me.pbxGrid.Name = "pbxGrid"
+        Me.pbxGrid.Size = New System.Drawing.Size(960, 479)
+        Me.pbxGrid.TabIndex = 5
+        Me.pbxGrid.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
-        Me.ClientSize = New System.Drawing.Size(1194, 572)
-        Me.Controls.Add(Me.pnlGrid)
+        Me.ClientSize = New System.Drawing.Size(984, 541)
+        Me.Controls.Add(Me.pbxGrid)
+        Me.Controls.Add(Me.tbxDebugLog)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.btnRight)
+        Me.Controls.Add(Me.btnLeft)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "Form1"
-        Me.Text = "Form1"
-        Me.pnlGrid.ResumeLayout(False)
+        Me.Text = "Piano Roll"
+        CType(Me.pbxGrid, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents pnlGrid As System.Windows.Forms.Panel
-    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents btnLeft As System.Windows.Forms.Button
+    Friend WithEvents btnRight As System.Windows.Forms.Button
+    Friend WithEvents btnPlay As System.Windows.Forms.Button
+    Friend WithEvents tbxDebugLog As System.Windows.Forms.TextBox
+    Friend WithEvents pnlNotes As System.Windows.Forms.Panel
+    Friend WithEvents pbxGrid As System.Windows.Forms.PictureBox
 
 End Class
--- Piano Roll/Form1.vb
+++ Piano Roll/Form1.vb
@@ -1,7 +1,12 @@
-﻿Imports WMPLib 'Media Player in virtual form
-Imports System.IO
+﻿Imports System.IO
 
+'Holds note values in hz
 Enum Note
+    B7 = 3951
+    B6 = 1976
+    Ash6 = 1865
+    A6 = 1760
+    B5 = 988
     Gsh5 = 831
     G5 = 784
     Fsh5 = 740
@@ -14,6 +19,8 @@ Enum Note
     B4 = 494
     Ash4 = 466
     A4 = 440
+    B3 = 247
+    B2 = 123
 End Enum
 
 Public Class Form1
@@ -23,77 +30,155 @@ Public Class Form1
     'Play a piano on the piano roll
 
     Const shtBlockSizeX As Short = 8
-    Const shtBlockSizeY As Short = 32
-    Const shtSpaceToLeft As Short = 0
-    Const shtSpaceToTop As Short = 0
+    Const shtBlockSizeY As Short = 16
 
-    Private arrayValue(512, 60) As Integer '1 is start of note, 2 is body, 3 is end, 0 is no note.
+    Private currentFileDirectory As String = Directory.GetCurrentDirectory.Remove(Directory.GetCurrentDirectory.IndexOf("\bin\Debug"), 10) + "\"
+
+    Private imgBackgroundTiles As Image = Image.FromFile(currentFileDirectory & "Grid of tiles.png")
+    Private imgRedTile As Image = Image.FromFile(currentFileDirectory & "RedTile.png")
+    Private imgBrownTile As Image = Image.FromFile(currentFileDirectory & "BrownTile.png")
+    Private imgTealTile As Image = Image.FromFile(currentFileDirectory & "TealTile.png")
+
+    Private arrayValue(511, 59) As Integer '1 is start of note, 2 is body, 3 is end, 0 is no note.
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Refresh()
 
     End Sub
 
-    Private Sub MouseDownClick(ByVal sender As System.Object, ByVal e As MouseEventArgs) Handles pnlGrid.MouseDown
+    Private Sub PaintNotes(ByVal o As Object, ByVal e As PaintEventArgs) Handles pbxGrid.Paint
+        e.Graphics.DrawImage(Image.FromFile(currentFileDirectory & "Grid of tiles.png"), Point.Empty)
+
+        'Problem VVV
+        For xPos As Integer = 0 To 128 'goes through each value and sets the colour
+            For yPos As Integer = 0 To 30
+                If arrayValue(xPos, yPos) = 0 Then
+                    Continue For
+                End If
+                If arrayValue(xPos, yPos) = 1 Then
+                    e.Graphics.DrawImage(imgRedTile, New Point(xPos * shtBlockSizeX, yPos * shtBlockSizeY))
+                ElseIf arrayValue(xPos, yPos) = 2 Then
+                    e.Graphics.DrawImage(imgBrownTile, New Point(xPos * shtBlockSizeX, yPos * shtBlockSizeY))
+                ElseIf arrayValue(xPos, yPos) = 3 Then
+                    e.Graphics.DrawImage(imgTealTile, New Point(xPos * shtBlockSizeX, yPos * shtBlockSizeY))
+                End If
+            Next yPos
+        Next xPos
+    End Sub
+
+    Private Sub MouseDownClick(ByVal sender As System.Object, ByVal e As MouseEventArgs) Handles pbxGrid.MouseDown
+
         If e.Button = Windows.Forms.MouseButtons.Left Then
-            Dim xIndex = (e.X + shtSpaceToLeft) \ shtBlockSizeX
-            Dim yIndex = (e.Y + shtSpaceToTop) \ shtBlockSizeY
+            Dim xIndex = (e.X + 0) \ shtBlockSizeX
+            Dim yIndex = (e.Y + 0) \ shtBlockSizeY
             Me.Text = "X: " & xIndex & " Y: " & yIndex
 
-            If yIndex = 0 Then
-                PlaySound(7000, 50)
-            ElseIf yIndex = 1 Then
-                PlaySound(Note.E5, 50)
-            ElseIf yIndex = 2 Then
-                PlaySound(Note.Dsh5, 50)
-            ElseIf yIndex = 3 Then
-                PlaySound(Note.D5, 50)
-            ElseIf yIndex = 4 Then
-                PlaySound(Note.Csh5, 50)
-            ElseIf yIndex = 5 Then
-                PlaySound(Note.C5, 50)
-            ElseIf yIndex = 6 Then
-                PlaySound(Note.B4, 50)
-            ElseIf yIndex = 7 Then
-                PlaySound(Note.Ash4, 50)
-            ElseIf yIndex = 8 Then
-                PlaySound(Note.A4, 50)
+            'tests
+            Select Case (yIndex)
+                Case 0
+                    PlaySound(Note.B7, 50)
+                Case 1
+                    PlaySound(Note.B6, 50)
+                Case 2
+                    PlaySound(Note.Ash6, 50)
+                Case 3
+                    PlaySound(Note.A6, 50)
+                Case 4
+                    PlaySound(Note.B5, 50)
+                Case 5
+                Case 6
+                Case 7
+                Case 8
+                Case 9
+                Case 10
+                Case 11
+                Case 12
+            End Select
+
+            If arrayValue(xIndex, yIndex) = 0 Then
+                tbxDebugLog.Text += " 0"
+                'places a note at the specific place clicked
+                FindAndDeleteNote(xIndex + 1, yIndex)
+                arrayValue(xIndex + 1, yIndex) = 3
+                arrayValue(xIndex, yIndex) = 1
             End If
-
-            'places a note at the specific place clicked
-            'FindAndDeleteNote(xIndex, yIndex)
-            arrayValue(xIndex, yIndex) = 1
-
-            'FindAndDeleteNote(xIndex + 1, yIndex)
-            arrayValue(xIndex + 1, yIndex) = 3
         End If
+        Refresh()
     End Sub
 
+    ''' <summary>
+    ''' Looks for a note in the containing space and deletes all reference of it.
+    ''' </summary>
     Private Sub FindAndDeleteNote(ByVal xIndex As Short, ByVal yIndex As Short)
         Dim shtCurrentPlace As Short = 0
         If arrayValue(xIndex, yIndex) <> 0 Then
+            'check down the row.  If it is a part of a note, delete it.  Stop when it hits the end of the note.
+
             If arrayValue(xIndex, yIndex) = 1 Then
-                While arrayValue(xIndex + shtCurrentPlace, yIndex) <> 3  'check down the row if it is a part of a note and delete it.
+                While arrayValue(xIndex + shtCurrentPlace, yIndex) <> 3
                     arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
                     shtCurrentPlace += 1
+
                 End While
                 arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
 
             ElseIf arrayValue(xIndex, yIndex) = 2 Then
+                'check up the row.  If it is a part of a note, delete it.  
+                'When it hits the start of the note go back to the middle and start down the note until it hits the end of the note. 
+
+                'deletes the 3
+                arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
+                shtCurrentPlace -= 1
+
                 While arrayValue(xIndex + shtCurrentPlace, yIndex) <> 0
                     If arrayValue(xIndex + shtCurrentPlace, yIndex) = 2 Then
                         arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
                         shtCurrentPlace -= 1
+
                     ElseIf arrayValue(xIndex + shtCurrentPlace, yIndex) = 1 Then
+                        arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
+                        shtCurrentPlace = 0
+                        Exit While
 
                     End If
-
                 End While
 
+                While arrayValue(xIndex + shtCurrentPlace, yIndex) <> 0
+                    If arrayValue(xIndex + shtCurrentPlace, yIndex) = 2 Then
+                        arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
+                        shtCurrentPlace += 1
+
+                    ElseIf arrayValue(xIndex + shtCurrentPlace, yIndex) = 3 Then
+                        arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
+                        Exit While
+
+                    End If
+                End While
+
+            ElseIf arrayValue(xIndex, yIndex) = 3 Then
+                'Check up the row.  If it is a part of a note, delete it.  When it hits the start of the note stop. 
+
+                'deletes the 3
+                arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
+                shtCurrentPlace -= 1
+
+                While arrayValue(xIndex + shtCurrentPlace, yIndex) <> 0
+                    If arrayValue(xIndex + shtCurrentPlace, yIndex) = 2 Then
+                        arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
+                        shtCurrentPlace -= 1
+
+                    ElseIf arrayValue(xIndex + shtCurrentPlace, yIndex) = 1 Then
+                        arrayValue(xIndex + shtCurrentPlace, yIndex) = 0
+                        shtCurrentPlace = 0
+                        Exit While
+
+                    End If
+                End While
             End If
         End If
     End Sub
 
-    Private Sub Button1Click() Handles Button1.Click
+    Private Sub Button1Click() Handles btnLeft.Click
         PlaySound(Note.A4, 50)
     End Sub
 
@@ -103,6 +188,10 @@ Public Class Form1
     ''' <param name="length">Input a number like 7/32</param>
     Private Sub PlaySound(ByVal note As Note, ByVal length As Short)
         Console.Beep(note, length)
+    End Sub
+
+    Private Sub Button1Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLeft.Click
+
     End Sub
 End Class
 
