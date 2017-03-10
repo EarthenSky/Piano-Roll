e739ce41095ebbdf4aa13be6ad094ecd66e859c3
EarthenSky <gabegames27@gmail.com>
2017-03-10 00:32:22 -0800

    added more note logic, amde it actually do stuff :D

--- Piano Roll/Form1.vb
+++ Piano Roll/Form1.vb
@@ -2,6 +2,15 @@
 Imports System.IO
 
 Enum Note
+    Gsh5 = 831
+    G5 = 784
+    Fsh5 = 740
+    F5 = 698
+    E5 = 659
+    Dsh5 = 622
+    D5 = 587
+    Csh5 = 554
+    C5 = 523
     B4 = 494
     Ash4 = 466
     A4 = 440
@@ -13,7 +22,10 @@ Public Class Form1
     '
     'Play a piano on the piano roll
 
-    Const shtBlockSize As Short = 8
+    Const shtBlockSizeX As Short = 8
+    Const shtBlockSizeY As Short = 32
+    Const shtSpaceToLeft As Short = 0
+    Const shtSpaceToTop As Short = 0
 
     Private arrayValue(512, 60) As Integer '1 is start of note, 2 is body, 3 is end, 0 is no note.
 
@@ -23,13 +35,36 @@ Public Class Form1
 
     Private Sub MouseDownClick(ByVal sender As System.Object, ByVal e As MouseEventArgs) Handles pnlGrid.MouseDown
         If e.Button = Windows.Forms.MouseButtons.Left Then
-            Dim xIndex = e.X + 6 \ shtBlockSize
-            Dim yIndex = e.Y + 6 \ shtBlockSize
+            Dim xIndex = (e.X + shtSpaceToLeft) \ shtBlockSizeX
+            Dim yIndex = (e.Y + shtSpaceToTop) \ shtBlockSizeY
+            Me.Text = "X: " & xIndex & " Y: " & yIndex
 
-            FindAndDeleteNote(xIndex, yIndex)
+            If yIndex = 0 Then
+                PlaySound(Note.F5, 50)
+            ElseIf yIndex = 1 Then
+                PlaySound(Note.E5, 50)
+            ElseIf yIndex = 2 Then
+                PlaySound(Note.Dsh5, 50)
+            ElseIf yIndex = 3 Then
+                PlaySound(Note.D5, 50)
+            ElseIf yIndex = 4 Then
+                PlaySound(Note.Csh5, 50)
+            ElseIf yIndex = 5 Then
+                PlaySound(Note.C5, 50)
+            ElseIf yIndex = 6 Then
+                PlaySound(Note.B4, 50)
+            ElseIf yIndex = 7 Then
+                PlaySound(Note.Ash4, 50)
+            ElseIf yIndex = 8 Then
+                PlaySound(Note.A4, 50)
+
+            End If
+
+            'places a note at the specific place clicked
+            'FindAndDeleteNote(xIndex, yIndex)
             arrayValue(xIndex, yIndex) = 1
 
-            FindAndDeleteNote(xIndex + 1, yIndex)
+            'FindAndDeleteNote(xIndex + 1, yIndex)
             arrayValue(xIndex + 1, yIndex) = 3
         End If
     End Sub
@@ -44,9 +79,10 @@ Public Class Form1
         End If
     End Sub
 
-    Private Sub Button1Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        PlaySound(Note.A4, 100)
+    Private Sub Button1Click() Handles Button1.Click
+        PlaySound(Note.A4, 50)
     End Sub
+
     ''' <summary>
     ''' Plays a sound based on the note and the length
     ''' </summary>
